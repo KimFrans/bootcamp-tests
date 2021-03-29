f6401886e3e99411068de7dad674e60448e67a03
Kim Frans <kimfrans34@gmail.com>
2021-03-29 15:29:57 +0200

    new review changes

--- tests/findItemsOver.test.js
+++ tests/findItemsOver.test.js
@@ -1,10 +1,10 @@
 describe('findItemsOver' , function(){
-    it('Should display the items that ahve a higher value than the threshold' , function(){
-        assert.deepEqual([], findItemsOver(["product:apples,qty:10","product:pears,qty:37", "25"]));
+    it('Should display the items that have a higher value than the threshold' , function(){
+        assert.deepEqual([{product:"pears",qty:37}] , findItemsOver([{product:"apples",qty:10},{product:"pears",qty:37}], 25));
 
     });
     it('Should display the items that have a value bigger than the threshold' , function(){
-        assert.deepEqual([], findItemsOver(["product:apples,qty:40","product:pears,qty:20", "30"]));
+        assert.deepEqual([{product:"apples",qty:40}] , findItemsOver([{product:"apples",qty:40},{product:"pears",qty:20}], 30));
 
     });
 
--- tests/findItemsOver20.test.js
+++ tests/findItemsOver20.test.js
@@ -1,10 +1,10 @@
 describe('findItemsOver20' , function(){
     it('Should display the items that have a quantity over 20' , function(){
-        assert.deepEqual([] , findItemsOver20(["product:apples,qty:10","product:pears,qty:37"]));
+        assert.deepEqual([{product:"pears",qty:37}] , findItemsOver20([{product:"apples",qty:10},{product:"pears",qty:37}]));
 
     });
     it('Should display the items that have a quantity higher than 20' , function(){
-        assert.deepEqual([] , findItemsOver20(["product:apples,qty:25","product:pears,qty:30"]));
+        assert.deepEqual([{product:"pears",qty:30}] , findItemsOver20([{product:"apples",qty:10},{product:"pears",qty:30}]));
 
     });
 
